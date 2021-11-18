23d58821ee193b7384f8bb08cce2b8659a7f967c
Jameson Hodge <contact@jamesonhodge.com>
2021-11-18 07:40:29 -0500

    feat: create tests for price FluxPriceFeed

---
+++ test/pricefeed/FluxPriceFeed.ts
@@ -0,0 +1,34 @@
+import { artifacts, ethers, waffle } from "hardhat";
+import type { Artifact } from "hardhat/types";
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+import type { FluxPriceFeed } from "../../src/types/FluxPriceFeed";
+import { Signers } from "../types";
+import { shouldBehaveLikeFluxPriceFeed } from "./FluxPriceFeed.behavior";
+
+describe("Unit tests", function () {
+  before(async function () {
+    this.signers = {} as Signers;
+
+    const signers: SignerWithAddress[] = await ethers.getSigners();
+    this.signers.admin = signers[0];
+    this.signers.nonadmin = signers[1];
+  });
+
+  describe("FluxPriceFeed", function () {
+    beforeEach(async function () {
+      const decimals: number = 6;
+      const description: string = "My description";
+      const pricefeedArtifact: Artifact = await artifacts.readArtifact("FluxPriceFeed");
+      this.pricefeed = <FluxPriceFeed>(
+        await waffle.deployContract(this.signers.admin, pricefeedArtifact, [
+          this.signers.admin.address,
+          decimals,
+          description,
+        ])
+      );
+    });
+
+    shouldBehaveLikeFluxPriceFeed();
+  });
+});
--- test/types.ts
+++ test/types.ts
@@ -1,11 +1,13 @@
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 import type { Fixture } from "ethereum-waffle";
 
-import type { Greeter } from "../src/types/Greeter";
+import type { FluxPriceFeed } from "../src/types/FluxPriceFeed";
+
+export const TEST_VALUE = 12345;
 
 declare module "mocha" {
   export interface Context {
-    greeter: Greeter;
+    pricefeed: FluxPriceFeed;
     loadFixture: <T>(fixture: Fixture<T>) => Promise<T>;
     signers: Signers;
   }
@@ -13,4 +15,5 @@ declare module "mocha" {
 
 export interface Signers {
   admin: SignerWithAddress;
+  nonadmin: SignerWithAddress;
 }
