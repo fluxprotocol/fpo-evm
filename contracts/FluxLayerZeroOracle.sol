e6282cc165f42ee43b091f34e174ce2882a92755
Jameson Hodge <contact@jamesonhodge.com>
2022-01-25 19:54:24 -0800

    feat: refactoring, cleanup LayerZero oracle

--- contracts/FluxLayerZeroOracle.sol
+++ contracts/FluxLayerZeroOracle.sol
@@ -1,114 +1,123 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import "./interface/ILayerZeroOracle.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-
-interface LayerZeroContract {
-    function updateBlockHeader(
-        uint16 remoteChainId,
-        address oracle,
-        bytes calldata blockHash,
-        uint256 confirmations,
-        bytes calldata receiptsRoot
-    ) external;
-}
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /**
  * @title Flux LayerZero oracle
  * @author fluxprotocol.org
  */
-contract FluxPriceFeed is AccessControl {
+contract FluxPriceFeed is AccessControl, ILayerZeroOracle, ReentrancyGuard {
     using EnumerableSet for EnumerableSet.UintSet;
+    using SafeERC20 for IERC20;
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant LAYERZERO_ROLE = keccak256("LAYERZERO_ROLE");
 
     struct Request {
+        uint16 chainId;
+        bytes layerZeroAddress;
+        uint256 confirmations;
         uint256 requestedAtBlock;
-        uint256 requiredBlockConfirmations;
-        address layerZeroContract;
-        bytes blockHash;
-        bytes receiptsRoot;
-        bool notified;
     }
 
-    EnumerableSet.UintSet private pendingRequests;
     uint256 public numRequests = 0;
+    EnumerableSet.UintSet private pendingRequests;
     mapping(uint256 => Request) public requests;
+    mapping(uint16 => uint256) public chainPriceLookup;
 
     //
     // EVENTS
     //
 
-    event Requested(
+    event NotifyOracleOfBlock(
         uint256 indexed requestId,
-        address layerZeroContract,
+        uint16 chainId,
+        bytes layerZeroContract,
         uint256 requiredBlockConfirmations,
-        bytes blockHash,
-        bytes receiptsRoot
+        uint256 requestedAtBlock
     );
     event Notified(uint256 indexed requestId);
+    event WithdrawTokens(address token, address to, uint256 amount);
+    event Withdraw(address to, uint256 amount);
 
     //
     // CONSTRUCTOR
     //
 
-    constructor(address _admin) {
+    constructor(address _admin, address _layerZero) {
         _setupRole(ADMIN_ROLE, _admin);
+        _setupRole(LAYERZERO_ROLE, _layerZero);
     }
 
     //
     // EXTERNAL METHODS
     //
 
-    function startRequest(
-        address layerZeroContract,
-        uint256 requiredBlockConfirmations,
-        bytes32 blockHash,
-        bytes32 receiptsRoot
-    ) external {
-        require(hasRole(ADMIN_ROLE, msg.sender), "Only admin can start a request");
-        numRequests++;
-        Request memory request = requests[numRequests];
-        if (request.layerZeroContract == address(0)) {
-            request.layerZeroContract = layerZeroContract;
-            request.requiredBlockConfirmations = requiredBlockConfirmations;
-            request.requestedAtBlock = block.number;
-            request.blockHash = abi.encodePacked(blockHash);
-            request.receiptsRoot = abi.encodePacked(receiptsRoot);
-            request.notified = false;
-        }
-        requests[numRequests] = request; // add to requests mapping
-        pendingRequests.add(numRequests); // add to pending requests set
+    /// @notice called by LayerZero to initiate a request
+    function notifyOracleOfBlock(
+        uint16 dstChainId,
+        bytes calldata dstNetworkAddress,
+        uint256 blockConfirmations
+    ) external override {
+        require(hasRole(LAYERZERO_ROLE, msg.sender), "LayerZero only");
 
-        emit Requested(
-            numRequests,
-            request.layerZeroContract,
-            request.requiredBlockConfirmations,
-            request.blockHash,
-            request.receiptsRoot
-        );
+        Request memory request = Request(dstChainId, dstNetworkAddress, blockConfirmations, block.number);
+        numRequests++;
+        requests[numRequests] = request; // add to requests mapping
+
+        emit NotifyOracleOfBlock(numRequests, dstChainId, dstNetworkAddress, blockConfirmations, block.number);
     }
 
-    function notify(uint256 key) external {
-        Request memory request = requests[key];
-        require(request.layerZeroContract != address(0), "Request not initialized");
-        require(request.notified == false, "Request already notified");
-        uint256 confirmationsSinceRequest = block.number - request.requestedAtBlock;
-        require(confirmationsSinceRequest >= request.requiredBlockConfirmations, "Not enough confirmations");
+    /// @notice called by admin after updateBlockHeader() is called on LayerZero for an existing request
+    function proceedUpdateBlockHeader(uint256 requestIndex) external {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Admin only");
 
-        LayerZeroContract(request.layerZeroContract).updateBlockHeader(
-            uint16(block.chainid), // chain id
-            address(this), // oracle
-            request.blockHash, // block hash
-            confirmationsSinceRequest, // confirmations
-            request.receiptsRoot // receipts root
-        );
-        request.notified = true;
-        requests[key] = request; // update request in requests mapping
-        pendingRequests.remove(key); // remove from pending requests set
+        Request memory request = requests[requestIndex];
+        requests[requestIndex] = request; // update request in requests mapping
+        pendingRequests.remove(requestIndex); // remove from pending requests set
 
-        emit Notified(key);
+        emit Notified(requestIndex);
+    }
+
+    // owner can approve a token spender
+    function approveToken(
+        address _token,
+        address _spender,
+        uint256 _amount
+    ) external {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Admin only");
+        IERC20 token = IERC20(_token);
+        token.safeApprove(_spender, _amount);
+    }
+
+    // owner can withdraw native
+    function withdraw(address payable _to, uint256 _amount) public nonReentrant {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Admin only");
+        (bool success, ) = _to.call{ value: _amount }("");
+        require(success, "OracleClient: failed to withdraw");
+        emit Withdraw(_to, _amount);
+    }
+
+    // owner can withdraw tokens
+    function withdrawTokens(
+        address _token,
+        address _to,
+        uint256 _amount
+    ) public {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Admin only");
+        IERC20(_token).safeTransfer(_to, _amount);
+        emit WithdrawTokens(_token, _to, _amount);
+    }
+
+    // owner can set gas price
+    function setPrice(uint16 _destinationChainId, uint256 price) external {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Admin only");
+        chainPriceLookup[_destinationChainId] = price;
     }
 
     //
@@ -118,4 +127,25 @@ contract FluxPriceFeed is AccessControl {
     function getPendingRequests() internal view returns (uint256[] memory _pendingRequests) {
         return pendingRequests.values();
     }
+
+    // return whether this signing address is whitelisted
+    function isApproved(address oracleAddress) external view override returns (bool approved) {
+        return hasRole(ADMIN_ROLE, oracleAddress);
+    }
+
+    function getPrice(uint16 dstChainId) external view returns (uint256 priceInWei) {
+        return chainPriceLookup[dstChainId];
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if no other
+     * function in the contract matches the call data.
+     */
+    fallback() external payable {}
+
+    /**
+     * @dev Fallback function that delegates calls to the address returned by `_implementation()`. Will run if call data
+     * is empty.
+     */
+    receive() external payable {}
 }
---
+++ contracts/interface/ILayerZeroOracle.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity ^0.8.10;
+
+// LayerZero oracle interface.
+interface ILayerZeroOracle {
+    // the qty of native gas token (on source) for initiating the oracle with notifyOracleOfBlock()
+    function getPrice(uint16 dstChainId) external view returns (uint256 priceInWei);
+
+    // initiates the offchain oracle to do its job
+    function notifyOracleOfBlock(
+        uint16 dstChainId,
+        bytes calldata dstNetworkAddress,
+        uint256 blockConfirmations
+    ) external;
+
+    // return true if the address is allowed to call updateBlockHeader()
+    function isApproved(address oracleSigner) external view returns (bool approved);
+}
