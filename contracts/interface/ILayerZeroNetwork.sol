aaeae6df346038ccdb69f15b237e8118d3264ec6
Jameson Hodge <contact@jamesonhodge.com>
2022-02-10 17:50:40 -0500

    fix: remove an arg from updateBlockHeader LZ interface

--- contracts/interface/ILayerZeroNetwork.sol
+++ contracts/interface/ILayerZeroNetwork.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.8.10;
 interface ILayerZeroNetwork {
     function updateBlockHeader(
         uint16 _srcChainId,
-        address _oracle,
         bytes calldata _blockHash,
         uint256 _confirmations,
         bytes calldata _data
