24cc998e69ae81b7f6e5f532e67bbe5549bff5cc
mnaga <mennat.abuelnaga@gmail.com>
2022-02-22 22:36:29 +0200

    FLUX-79 changed transmit task to receive multiple args

--- tasks/multiPriceFeedTransmit.ts
+++ tasks/multiPriceFeedTransmit.ts
@@ -4,10 +4,12 @@ import { ethers } from "ethers";
 import { task } from "hardhat/config";
 // import { task } from "@nomiclabs/buidler/config";
 
+// npx hardhat multiPriceFeedTransmit --contract 0xf72c30D7c945f7Fbc659631D775D9a3402d61dC9 --network kovan --pricepairs "Price-ETH/USD-3 Price-BTC/USD-3" --answers "3000 37000"
+
 task("multiPriceFeedTransmit", "Submits an answer to multiPriceFeed")
   .addParam("contract", "The price feed contract to post to")
-  .addParam("pricepair", "The prices posted")
-  .addParam("answer", "The prices posted")
+  .addParam("pricepairs", "The prices posted")
+  .addParam("answers", "The prices posted")
   .setAction(async (_taskArgs, hre) => {
     const accounts: Signer[] = await hre.ethers.getSigners();
     const validator = accounts[0];
@@ -15,25 +17,23 @@ task("multiPriceFeedTransmit", "Submits an answer to multiPriceFeed")
     const FluxMultiPriceFeed = await hre.ethers.getContractFactory("contracts/FluxMultiPriceFeed.sol:FluxMultiPriceFeed");
     const contract = await FluxMultiPriceFeed.attach(_taskArgs.contract);
 
+    let received_pairs = _taskArgs.pricepairs.split(" ");
+    let received_answers = _taskArgs.answers.split(" ");
+
     let pairs = [];
-    let answers = [];
+    // let answers = [];
+    for(let pair of received_pairs){
+      pairs.push(ethers.utils.keccak256(ethers.utils.toUtf8Bytes(pair)));
 
-    let pair = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_taskArgs.pricepair));
-    pairs.push(pair)
+    }
     console.log(pairs);
+    console.log(received_answers);
 
-    answers.push(_taskArgs.answer);
-    
-    // console.log(_taskArgs.prices);
-
-    // ethers.utils.keccak256(ethers.utils.toUtf8Bytes(_taskArgs.answers[0]));
-    
-    
-    let tx = await contract.connect(validator).transmit(pairs, answers);
+    let tx = await contract.connect(validator).transmit(pairs, received_answers);
 
     console.log("Transaction hash:", tx.hash);
 
-    // tx = await contract.valueFor(ethers.utils.formatBytes32String(pair));
+    // tx = await contract.valueFor(pairs[0]);
     // console.log(tx);
 
   })
