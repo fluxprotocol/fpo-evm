ccf33a84cabd6eb5718a562e28cd59f16b218d02
mnaga <mennat.abuelnaga@gmail.com>
2022-02-25 04:14:35 +0200

    FLUX-122 changed fns in factory contract

--- contracts/FluxPriceFeedFactory.sol
+++ contracts/FluxPriceFeedFactory.sol
@@ -3,28 +3,65 @@ pragma solidity ^0.8.10;
 
 import "./interface/IERC2362.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "./FluxMultiPriceFeed.sol";
+// import "./FluxMultiPriceFeed.sol";
 import "./FluxPriceFeed.sol";
 /**
  * @title Flux first-party price feed factory
  * @author fluxprotocol.org
  */
-contract FluxPriceFeedFactory is AccessControl {
+contract FluxPriceFeedFactory is AccessControl, IERC2362 {
     bytes32 public constant VALIDATOR_ROLE = keccak256("VALIDATOR_ROLE");
 
-    FluxPriceFeed[] public FluxPriceFeedsArray;
-    FluxMultiPriceFeed[] public FluxMultiPriceFeedsArray;
-    
-
+    bytes32[] public FluxPriceFeedsArray;
     mapping(bytes32 => FluxPriceFeed) public FluxPriceFeedsMapping;
-    // mapping(bytes32 => FluxMultiPriceFeed) public FluxMultiPriceFeedsMapping;
+
+
     
+    struct PricePair {
+        int256 price;
+        uint256 timestamp;
+    }
+    mapping(bytes32 => PricePair) public FluxMultiPriceFeedsMapping;
+    bytes32[] public FluxMultiPriceFeedsArray;
 
     constructor(address _validator) {
         _setupRole(VALIDATOR_ROLE, _validator);
     }
 
+     /**
+     * @notice answer from the most recent report of a certain price pair
+     * @param _id Keccak256 hash of the price pair string we wanna query
+     */
+    function valueFor(bytes32 _id)
+        external
+        view
+        override
+        returns (
+            int256,
+            uint256,
+            uint256
+        )
+    {
+        // if not found, return 404
+        if (FluxMultiPriceFeedsMapping[_id].timestamp <= 0) return (0, 0, 404);
 
+        return (FluxMultiPriceFeedsMapping[_id].price, FluxMultiPriceFeedsMapping[_id].timestamp, 200);
+    }
+
+    /**
+     * @notice transmit is called to post a new value to the contract price pairs
+     * @param _pricePairs array of price pairs ids we wanna post values for
+     * @param _answers array of prices we wanna post
+
+     */
+    function transmit(bytes32[] calldata _pricePairs, int256[] calldata _answers) external onlyRole(VALIDATOR_ROLE) {
+        require(_answers.length == _pricePairs.length, "The transmitted arrays must be equal");
+        for (uint256 i = 0; i < _pricePairs.length; i++) {
+            FluxMultiPriceFeedsArray.push(_pricePairs[i]);
+            FluxMultiPriceFeedsMapping[_pricePairs[i]].price = _answers[i];
+            FluxMultiPriceFeedsMapping[_pricePairs[i]].timestamp = block.timestamp;
+        }
+    }
     
    
 
@@ -34,8 +71,9 @@ contract FluxPriceFeedFactory is AccessControl {
         public returns (address)
     {
         FluxPriceFeed priceFeed = new FluxPriceFeed(_validator, _decimals, _description);
-        FluxPriceFeedsArray.push(priceFeed);
+        // FluxPriceFeedsArray.push(priceFeed);
         bytes32 _id = keccak256(abi.encodePacked(_description));
+        FluxPriceFeedsArray.push(_id);
         FluxPriceFeedsMapping[_id] = priceFeed;
         return address(priceFeed);
 
@@ -44,7 +82,7 @@ contract FluxPriceFeedFactory is AccessControl {
     /**
      * @notice answer from the most recent report
      */
-    function priceFeedLatestAnswer(string memory _description) public returns (int256) {
+    function priceFeedLatestAnswer(string memory _description) public view returns (int256) {
         return FluxPriceFeed(address(FluxPriceFeedsMapping[keccak256(abi.encodePacked(_description))])).latestAnswer();
 
     }
@@ -58,70 +96,16 @@ contract FluxPriceFeedFactory is AccessControl {
 
     }
 
-
-
-
-
-
-    /// @notice create new multiPriceFeed
-    function CreateNewMultiPriceFeed(address _validator) public returns (address){
-        FluxMultiPriceFeed multiPricFeed = new FluxMultiPriceFeed(_validator);
-        FluxMultiPriceFeedsArray.push(multiPricFeed);
-        return address(multiPricFeed);
+    function listPriceFeeds() public view returns(bytes32[] memory){
+        return FluxPriceFeedsArray;
+        
     }
 
-    /**
-     * @notice answer from the most recent report of a certain price pair
-     * @param _id Keccak256 hash of the price pair string we wanna query
-     */
-    function multiPriceFeedValueFor(uint256 _FluxMultiPriceFeedIndex, bytes32 _id) public
-        returns (
-            int256,
-            uint256,
-            uint256
-        )
-    {
-        return FluxMultiPriceFeed(address(FluxMultiPriceFeedsArray[_FluxMultiPriceFeedIndex])).valueFor(_id);
-      
-    }
- 
-
-
-    function multiPriceFeedTransmit(uint256 _FluxMultiPriceFeedIndex, 
-        bytes32[] calldata _pricePairs, int256[] calldata _answers) public onlyRole(VALIDATOR_ROLE)
-    {
-        FluxMultiPriceFeed(address(FluxMultiPriceFeedsArray[_FluxMultiPriceFeedIndex])).transmit(_pricePairs, _answers);
-
+    function listMutiPricePairs() public view returns(bytes32[] memory){
+        return FluxMultiPriceFeedsArray;
+        
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 
 }
