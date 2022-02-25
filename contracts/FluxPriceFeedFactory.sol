986b5728e1a29ca23116e7206eb62cdf11b868f8
mnaga <mennat.abuelnaga@gmail.com>
2022-02-26 01:15:29 +0200

    FLUX-122 added tests

--- contracts/FluxPriceFeedFactory.sol
+++ contracts/FluxPriceFeedFactory.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.10;
 import "./interface/IERC2362.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./FluxPriceFeed.sol";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * @title Flux first-party price feed factory
@@ -16,6 +18,7 @@ contract FluxPriceFeedFactory is AccessControl, IERC2362 {
     mapping(bytes32 => FluxPriceFeed) public fluxPriceFeeds;
 
     event FluxPriceFeedCreated(bytes32 indexed id, address indexed oracle);
+    event Log(string message);
 
     constructor(address _validator) {
         _setupRole(VALIDATOR_ROLE, _validator);
@@ -32,22 +35,43 @@ contract FluxPriceFeedFactory is AccessControl, IERC2362 {
         uint8[] calldata _decimals,
         int192[] calldata _answers
     ) external onlyRole(VALIDATOR_ROLE) {
+        require((_pricePairs.length == _decimals.length) && (_pricePairs.length == _answers.length), "Transmitted arrays must be equal");
         // Iterate through each transmitted price pair
         for (uint256 i = 0; i < _pricePairs.length; i++) {
             // Find the price pair id
-            bytes32 id = keccak256(abi.encodePacked("Price-", _pricePairs[i], "-", _decimals[i]));
+            string memory str = string(abi.encodePacked("Price-", _pricePairs[i], "-", Strings.toString(_decimals[i])));
+            console.log(str);
+            bytes32 id = keccak256(bytes(str));
+            
+            // if oracle exists then transmit values
+            if( address(fluxPriceFeeds[id]) != address(0x0) ){
+                // try to transmit or create new price pair if it doesn't exist
+                try fluxPriceFeeds[id].transmit(_answers[i]) {
+                    // transmission is successful, nothing to do
+                    console.log("Transmitted values");
+                }catch Error(string memory reason) {
+                    // catch failing revert() and require()
+                    console.log(reason);
+                }
+            // else create an oracle then transmit values
+            }else{
 
-            // try to transmit or create new price pair if it doesn't exist
-            try fluxPriceFeeds[id].transmit(_answers[i]) {
-                // transmission is successful, nothing to do
-            } catch {
                 // transmission failed, let's create a new price feed and try transmitting again
-                FluxPriceFeed newPriceFeed = new FluxPriceFeed(msg.sender, _decimals[i], _pricePairs[i]);
+                // FluxPriceFeed newPriceFeed = new FluxPriceFeed(msg.sender, _decimals[i], _pricePairs[i]);
+
+                // Should we let the factory be the validator or let the sender grantRole before calling transmit?
+                FluxPriceFeed newPriceFeed = new FluxPriceFeed(address(this), _decimals[i], _pricePairs[i]);
                 fluxPriceFeeds[id] = newPriceFeed;
+                console.log("++Created Oracle");
+                console.logBytes32(id);
                 emit FluxPriceFeedCreated(id, address(newPriceFeed));
 
                 fluxPriceFeeds[id].transmit(_answers[i]);
+                console.log("++Transmitted values");
+
             }
+
+         
         }
     }
 
@@ -65,10 +89,22 @@ contract FluxPriceFeedFactory is AccessControl, IERC2362 {
             uint256
         )
     {
-        // fetch the price feed contract and read its latest answer and timestamp
-        try fluxPriceFeeds[_id].latestTransmissionDetails() returns (int192 _latestAnswer, uint64 _latestTimestamp) {
-            return (_latestAnswer, _latestTimestamp, 200);
-        } catch {
+        // if oracle exists then fetch values
+        if( address(fluxPriceFeeds[_id]) != address(0x0) ){
+            // fetch the price feed contract and read its latest answer and timestamp
+            try fluxPriceFeeds[_id].latestTransmissionDetails() 
+                returns (int192 _latestAnswer, uint64 _latestTimestamp) {
+
+                return (_latestAnswer, _latestTimestamp, 200);
+
+            }catch Error(string memory reason) {
+                // catch failing revert() and require()
+                console.log(reason);
+            }
+        // else return not found
+        }else{
+            console.log("No oracle was found");
+            console.logBytes32(_id);
             return (0, 0, 404);
         }
     }
---
+++ test/factory/FluxPriceFeedFactory.behavior.ts
@@ -0,0 +1,52 @@
+import { expect } from "chai";
+import { TEST_VALUE } from "../types";
+
+export function shouldBehaveLikeFluxPriceFeedFactory(): void {
+  it("should transmit arrays and return values", async function () {
+    let [price, timeStamp, status] = await this.factory.connect(this.signers.admin).valueFor(this.eth_usd_id);
+    expect(price).to.equal(0);
+    expect(timeStamp).to.equal(0);
+    expect(status).to.equal(404);
+    let pricePairs = [this.eth_usd_str, this.btc_usd_str];
+    let decimals = [3, 3];
+    let answers = [3000, 37600];
+    // console.log("validator = ", this.signers.admin.address);
+    await this.factory.connect(this.signers.admin).transmit(pricePairs, decimals, answers);
+    [price, , status] = await this.factory.connect(this.signers.admin).valueFor(this.eth_usd_id);
+    expect(price).to.equal(3000);
+    expect(status).to.equal(200);
+    [price, , status] = await this.factory.connect(this.signers.admin).valueFor(this.btc_usd_id);
+    expect(price).to.equal(37600);
+    expect(status).to.equal(200);
+  });
+  it("should overwrite values", async function () {
+    let pricePairs = [this.eth_usd_str, this.btc_usd_str];
+    let decimals = [3, 3];
+    let answers = [3000, 37600];
+    await this.factory.connect(this.signers.admin).transmit(pricePairs, decimals, answers);
+    let [price, , status] = await this.factory.connect(this.signers.admin).valueFor(this.eth_usd_id);
+    expect(price).to.equal(3000);
+    expect(status).to.equal(200);
+    answers = [2500, 37000];
+    await this.factory.connect(this.signers.admin).transmit(pricePairs, decimals, answers);
+    [price, , status] = await this.factory.connect(this.signers.admin).valueFor(this.eth_usd_id);
+    expect(price).to.equal(2500);
+    expect(status).to.equal(200);
+  });
+
+  it("should revert if caller is not a validator", async function () {
+    let pricePairs = [this.eth_usd_str, this.btc_usd_str];
+    let decimals = [3, 3];
+    let answers = [3000, 37600];
+    await expect(this.factory.connect(this.signers.nonadmin).transmit(pricePairs, decimals, answers)).to.be.reverted;
+  });
+
+  it("should revert if transmitted arrays aren't equal", async function () {
+    let pricePairs = [this.eth_usd_str, this.btc_usd_str];
+    let decimals = [3, 3];
+    let answers = [3000];
+    await expect(this.factory.connect(this.signers.admin).transmit(pricePairs, decimals, answers)).to.be.revertedWith(
+      "Transmitted arrays must be equal",
+    );
+  });
+}
