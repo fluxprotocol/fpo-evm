64ad0f5ebcbc0917612f4d4173e06a806523e0fa
Jameson Hodge <contact@jamesonhodge.com>
2022-03-08 08:21:57 -0500

    fix: remove unused var caught by lint

--- tasks/typeAndVersion.ts
+++ tasks/typeAndVersion.ts
@@ -4,7 +4,6 @@ import { task } from "hardhat/config";
 task("typeAndVersion", "Fetches the type and version of the contract")
   .addParam("contract")
   .setAction(async (_taskArgs, hre) => {
-    const accounts: Signer[] = await hre.ethers.getSigners();
     const FluxPriceFeed = await hre.ethers.getContractFactory("contracts/FluxPriceFeed.sol:FluxPriceFeed");
     const contract = await FluxPriceFeed.attach(_taskArgs.contract);
 
