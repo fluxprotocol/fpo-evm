1fce8bf54d14189af1a194002ddc1163e734d5ab
jasperdg <jasperdegooijer@gmail.com>
2022-03-09 18:23:16 -0600

    fix: retry on empty block

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,12 +1,12 @@
-echo "depoloying rinkeby"
-npx hardhat deploy:FluxLayerZeroOracle  --network rinkeby --layerzero 0x40d385859663c82763E5287774C8a6e3823f9DD5
+echo "deploying rinkeby"
+npx hardhat deploy:FluxLayerZeroOracle  --network rinkeby --layerzero 0xC243d1216dE99aF3372395153f1b1Ed65Bd0c2CB
 echo "bsc"
-npx hardhat deploy:FluxLayerZeroOracle  --network bsc-testnet --layerzero 0x1F3fd8f7e722B8bEC8f43a70A583DFc0DbaCAC17
+npx hardhat deploy:FluxLayerZeroOracle  --network bsc-testnet --layerzero 0x7333D9a183ef484f0343135e029EfAA3eb7271a8
 echo "fuji"
-npx hardhat deploy:FluxLayerZeroOracle  --network fuji --layerzero 0x4B2FdCA1C3D7fd7075B669Aaa925C58eA6293b11
+npx hardhat deploy:FluxLayerZeroOracle  --network fuji --layerzero 0xc528020Cee8Dd7d736466987b4d8f9901a6A5A3e
 echo "mumbai"
-npx hardhat deploy:FluxLayerZeroOracle  --network mumbai --layerzero 0x51b9b86498afaD36F3500bc2989dEEF2b5B0ea05
+npx hardhat deploy:FluxLayerZeroOracle  --network mumbai --layerzero 0x17A329A42E7587E43128Ee8Afd059B8c2209A6E3
 echo "rinkeby arbitrum"
-npx hardhat deploy:FluxLayerZeroOracle  --network arbitrum-rinkeby --layerzero 0x75ECE223242810528f76EE266E0119081227156f
+npx hardhat deploy:FluxLayerZeroOracle  --network arbitrum-rinkeby --layerzero 0xf2fEa0253ce6263eBDaE4d3E92c5162D15A8BF5F
 echo "optimistic kovan"
-npx hardhat deploy:FluxLayerZeroOracle  --network optimism-kovan --layerzero 0x3B70EFBa8d608cf6992E15D6513106E7Fa58a4fb
+npx hardhat deploy:FluxLayerZeroOracle  --network optimism-kovan --layerzero 0x4EE70c13F24aAd9EE0F9995ab25C5a0BF99433Ae
