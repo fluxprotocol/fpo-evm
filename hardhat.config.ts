8c375d3fd97c96daea0e5f67a3efd76582405943
Jameson Hodge <contact@jamesonhodge.com>
2022-03-10 08:17:32 -0500

    feat: option of using mnemonic or private key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,34 +20,15 @@ import { resolve } from "path";
 
 import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
-import { NetworkUserConfig } from "hardhat/types";
 
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
-const ethChainIds = {
-  goerli: 5,
-  hardhat: 31337,
-  kovan: 42,
-  mainnet: 1,
-  rinkeby: 4,
-  ropsten: 3,
-};
-
-const mnemonic = process.env.MNEMONIC || "test test test test test test test test test test test junk";
+const mnemonic = process.env.MNEMONIC;
+// const mnemonic = process.env.MNEMONIC || "test test test test test test test test test test test junk";
+const privateKey = process.env.PRIVATE_KEY || "0x" + "0".repeat(64);
 const infuraApiKey = process.env.INFURA_API_KEY || "";
 
-function getChainConfig(network: keyof typeof ethChainIds): NetworkUserConfig {
-  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
-  return {
-    accounts: {
-      count: 10,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
-    chainId: ethChainIds[network],
-    url,
-  };
-}
+const accounts = process.env.PRIVATE_KEY ? [privateKey] : { mnemonic };
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -63,158 +44,176 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       accounts: {
+        count: 10,
         mnemonic,
+        path: "m/44'/60'/0'/0",
       },
-      chainId: ethChainIds.hardhat,
+      chainId: 31337,
     },
     arbitrum: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://arb1.arbitrum.io/rpc",
       chainId: 42161,
       blockGasLimit: 700000,
     },
     "arbitrum-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://kovan3.arbitrum.io/rpc",
       chainId: 79377087078960,
     },
     aurora: {
-      accounts: {
-        mnemonic,
-      },
+      accounts,
       chainId: 1313161554,
       url: "https://mainnet.aurora.dev",
     },
     "aurora-testnet": {
-      accounts: {
-        mnemonic,
-      },
+      accounts,
       chainId: 1313161555,
       url: "https://testnet.aurora.dev",
     },
     avalanche: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://api.avax.network/ext/bc/C/rpc",
       chainId: 43114,
       gasPrice: 470000000000,
     },
     "avalanche-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       chainId: 43113,
     },
     bsc: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://bsc-dataseed.binance.org",
       chainId: 56,
     },
     "bsc-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://data-seed-prebsc-2-s3.binance.org:8545",
       chainId: 97,
     },
     celo: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://forno.celo.org",
       chainId: 42220,
     },
     "celo-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://alfajores-forno.celo-testnet.org",
       chainId: 44787,
     },
     chronos: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://evm.cronos.org",
       chainId: 25,
     },
     "chronos-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://cronos-testnet-3.crypto.org:8545",
       chainId: 338,
     },
     fantom: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpcapi.fantom.network",
       chainId: 250,
     },
     "fantom-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpc.testnet.fantom.network",
       chainId: 4002,
     },
     harmony: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://api.s0.t.hmny.io",
       chainId: 1666600000,
     },
     "harmony-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://api.s0.b.hmny.io",
       chainId: 1666700000,
     },
     kucoin: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpc-mainnet.kcc.network",
       chainId: 321,
     },
     "kucoin-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpc-testnet.kcc.network",
       chainId: 322,
     },
     "matic-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://stardust.metis.io/?owner=588",
       chainId: 588,
     },
     metis: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://andromeda.metis.io/?owner=1088",
       chainId: 1088,
     },
     "metis-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://proxy.devnet.neonlabs.org/solana",
       chainId: 245022926,
     },
     neon: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://proxy.mainnet.neonlabs.org/solana",
       chainId: 245022934,
     },
     "neon-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://proxy.devnet.neonlabs.org/solana",
       chainId: 245022926,
     },
     polygon: {
-      accounts: { mnemonic },
+      accounts,
       url: `https://rpc-mainnet.maticvigil.com/`,
       chainId: 137,
     },
     gnosis: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpc.gnosischain.com",
       chainId: 100,
     },
     syscoin: {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpc.syscoin.org",
       chainId: 57,
       blockGasLimit: 8000000,
     },
     "syscoin-testnet": {
-      accounts: { mnemonic },
+      accounts,
       url: "https://rpc.tanenbaum.io",
       chainId: 5700,
       blockGasLimit: 8000000,
     },
-    goerli: getChainConfig("goerli"),
-    kovan: getChainConfig("kovan"),
-    rinkeby: getChainConfig("rinkeby"),
-    ropsten: getChainConfig("ropsten"),
-    mainnet: getChainConfig("mainnet"),
+    goerli: {
+      accounts,
+      url: "https://goerli.infura.io/v3/" + infuraApiKey,
+      chainId: 5,
+    },
+    kovan: {
+      accounts,
+      url: "https://kovan.infura.io/v3/" + infuraApiKey,
+      chainId: 42,
+    },
+    rinkeby: {
+      accounts,
+      url: "https://rinkeby.infura.io/v3/" + infuraApiKey,
+      chainId: 4,
+    },
+    ropsten: {
+      accounts,
+      url: "https://ropsten.infura.io/v3/" + infuraApiKey,
+      chainId: 3,
+    },
+    mainnet: {
+      accounts,
+      url: "https://mainnet.infura.io/v3/" + infuraApiKey,
+      chainId: 1,
+    },
   },
   paths: {
     artifacts: "./artifacts",
