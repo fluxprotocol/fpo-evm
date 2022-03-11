2946f182834dc1a5d3f16a8304754844e01adde1
mnaga <mennat.abuelnaga@gmail.com>
2022-03-11 14:17:06 +0200

    chore: add etherscan verification

--- tasks/deploy/multipricefeed.ts
+++ tasks/deploy/multipricefeed.ts
@@ -5,9 +5,14 @@ import { TaskArguments } from "hardhat/types";
 import { FluxMultiPriceFeed } from "../../src/types/FluxMultiPriceFeed";
 import { FluxMultiPriceFeed__factory } from "../../src/types/factories/FluxMultiPriceFeed__factory";
 
+import sleep from "../../utils/sleep";
+const VERIFY_DELAY = 100000;
+// npx hardhat deploy:FluxMultiPriceFeed --network kovan --verify
+// deployed and verified: 0x41c09D71874490AC5800998598be7623493Ce529
 task("deploy:FluxMultiPriceFeed")
   .addOptionalParam("validator", "The validator allowed to post data to the contract")
-  .setAction(async function (taskArgs: TaskArguments, { ethers }) {
+  .addFlag("verify")
+  .setAction(async function (taskArgs: TaskArguments, { run, ethers }) {
     const accounts: Signer[] = await ethers.getSigners();
 
     let validator;
@@ -24,4 +29,14 @@ task("deploy:FluxMultiPriceFeed")
     const multiPriceFeed: FluxMultiPriceFeed = <FluxMultiPriceFeed>await multiPricefeedFactory.deploy(validator);
     await multiPriceFeed.deployed();
     console.log("FluxMultiPriceFeed deployed to: ", multiPriceFeed.address);
+
+    if (taskArgs.verify) {
+      console.log("Verifying contract, can take some time");
+      await sleep(VERIFY_DELAY);
+      await run("verify:verify", {
+        address: multiPriceFeed.address,
+        constructorArguments: [validator],
+      });
+      console.log("Etherscan Verification Done");
+    }
   });
--- tasks/deploy/pricefeeds.ts
+++ tasks/deploy/pricefeeds.ts
@@ -5,11 +5,16 @@ import { TaskArguments } from "hardhat/types";
 import { FluxPriceFeed } from "../../src/types/FluxPriceFeed";
 import { FluxPriceFeed__factory } from "../../src/types/factories/FluxPriceFeed__factory";
 
+import sleep from "../../utils/sleep";
+const VERIFY_DELAY = 100000;
+// npx hardhat deploy:FluxPriceFeed --network kovan --decimals 18 --description "test price feed" --verify
+//deployed to: 0x44AAaD7Ab25eA77692586c6227d6Df67b7E7d85e
 task("deploy:FluxPriceFeed")
   .addParam("decimals", "The number of decimals in the value posted")
   .addParam("description", "The description of the contract")
   .addOptionalParam("validator", "The validator allowed to post data to the contract")
-  .setAction(async function (taskArgs: TaskArguments, { ethers }) {
+  .addFlag("verify")
+  .setAction(async function (taskArgs: TaskArguments, { run, ethers }) {
     const accounts: Signer[] = await ethers.getSigners();
 
     let validator;
@@ -27,4 +32,14 @@ task("deploy:FluxPriceFeed")
     );
     await pricefeed.deployed();
     console.log("FluxPriceFeed deployed to: ", pricefeed.address);
+
+    if (taskArgs.verify) {
+      console.log("Verifying contract, can take some time");
+      await sleep(VERIFY_DELAY);
+      await run("verify:verify", {
+        address: pricefeed.address,
+        constructorArguments: [validator, taskArgs.decimals, taskArgs.description],
+      });
+      console.log("Etherscan Verification Done");
+    }
   });
