45aac922b9aece4a098ec77a0600542e70129b5b
jamesondh <contact@jamesonhodge.com>
2022-03-15 12:22:15 -0400

    feat(tasks): show timestamp, decimals in latestAnswer task (#13)

--- tasks/latestAnswer.ts
+++ tasks/latestAnswer.ts
@@ -6,6 +6,14 @@ task("latestAnswer", "Fetches the latest answer")
     const FluxPriceFeed = await hre.ethers.getContractFactory("contracts/FluxPriceFeed.sol:FluxPriceFeed");
     const contract = await FluxPriceFeed.attach(_taskArgs.contract);
 
-    const tx = await contract.latestAnswer();
-    console.log(tx.toString());
+    const decimals = await contract.decimals();
+    const latestAnswer = await contract.latestAnswer();
+    const latestTimestamp = await contract.latestTimestamp();
+
+    const formattedAnswer = latestAnswer.toString() / 10 ** decimals;
+    const formattedTimestamp = new Date(latestTimestamp.toNumber() * 1000);
+
+    console.log(
+      `Latest answer is ${formattedAnswer} (raw value: ${latestAnswer.toString()}), last updated at ${formattedTimestamp}`,
+    );
   });
