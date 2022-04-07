ef6b61081e4f1f2292e39161c419cc67674bc812
mnaga <mennat.abuelnaga@gmail.com>
2022-04-07 19:36:10 +0200

    test: add more p2p tests

--- test/p2pmock/P2PFactory.behavior.ts
+++ test/p2pmock/P2PFactory.behavior.ts
@@ -54,6 +54,23 @@ export function shouldBehaveLikeFluxP2PFactory(): void {
     ).to.be.revertedWith("Signer must be a validator");
   });
 
+  it("should revert if answers aren't valid", async function () {
+    const pricePair = this.eth_usd_str;
+    const decimals = 3;
+    let answers = [3000, 4000];
+
+    // sign answer 0 by provider1 and answer 1 by provider2
+    let p1_msgHash = ethers.utils.solidityKeccak256(["string", "uint8", "int192"], [pricePair, decimals, answers[1]]);
+    let p2_msgHash = ethers.utils.solidityKeccak256(["string", "uint8", "int192"], [pricePair, decimals, answers[1]]);
+    let p1_sig = await this.nonprovider.signMessage(arrayify(p1_msgHash));
+    let p2_sig = await this.provider2.signMessage(arrayify(p2_msgHash));
+    let sigs = [p1_sig, p2_sig];
+
+    await expect(
+      this.factory.connect(this.signers.admin).transmit(sigs, pricePair, decimals, answers),
+    ).to.be.revertedWith("Signer must be a validator");
+  });
+
   it("should revert if transmitted arrays aren't equal", async function () {
     const pricePair = this.eth_usd_str;
     const decimals = 3;
@@ -207,5 +224,25 @@ export function shouldBehaveLikeFluxP2PFactory(): void {
     await expect(
       this.factory.connect(this.signers.nonadmin).revokeProvider(this.provider3tobe.address),
     ).to.be.revertedWith("NOT AN ADMIN, CANT REVOKE PROVIDER");
+  });
+
+  it("should transmit providers with diff orders", async function () {
+    const pricePair = this.eth_usd_str;
+    const decimals = 3;
+    let answers = [2000, 4000];
+
+    // sign answer 0 by provider1 and answer 1 by provider2
+    let p1_msgHash = ethers.utils.solidityKeccak256(["string", "uint8", "int192"], [pricePair, decimals, answers[1]]);
+    let p2_msgHash = ethers.utils.solidityKeccak256(["string", "uint8", "int192"], [pricePair, decimals, answers[0]]);
+    let p1_sig = await this.provider1.signMessage(arrayify(p1_msgHash));
+    let p2_sig = await this.provider2.signMessage(arrayify(p2_msgHash));
+    // let sigs = [p1_sig, p2_sig];
+    let sigs = [p2_sig, p1_sig];
+
+    await this.factory.connect(this.signers.admin).transmit(sigs, pricePair, decimals, answers);
+
+    let [price, , status] = await this.factory.connect(this.signers.admin).valueFor(this.eth_usd_id);
+    expect(price).to.equal(3000);
+    expect(status).to.equal(200);
   });
 }
