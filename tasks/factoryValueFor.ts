f94c30e6a13217ccaf71923dcd948ceb837721e0
mnaga <mennat.abuelnaga@gmail.com>
2022-04-13 11:09:54 +0200

    fix: fix factoryValueFor task

--- tasks/factoryValueFor.ts
+++ tasks/factoryValueFor.ts
@@ -2,8 +2,7 @@ import { utils } from "ethers";
 
 import { task } from "hardhat/config";
 
-// npx hardhat factoryValueFor --network kovan --contract 0x508086f87681A0724cA1a1C7a50ABFB79E7d2F64 --pricepairs "Price-ETH/USD-3 Price-BTC/USD-3"
-
+// npx hardhat factoryValueFor --network kovan --contract 0xe9D3Fe180604e38eec08F8179Be82c3fCd1219a2 --pricepairs "Price-ETH/USD-6" --provider 0xE19E8d5346Ade8294ec07c5431E5f6A1bb7F8ab2
 task("factoryValueFor", "fetches oracle address")
   .addParam("contract", "The factory contract address")
   .addParam("pricepairs", "Price pair to query")
