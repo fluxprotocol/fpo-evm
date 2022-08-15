96a8ff2fdd16ce93885f048271924fd9264ec48f
Jameson Hodge <contact@jamesonhodge.com>
2022-08-15 12:17:02 -0400

    feat(RelayerConsumer): check if any rounds have been posted

--- contracts/RelayerConsumer.sol
+++ contracts/RelayerConsumer.sol
@@ -17,11 +17,15 @@ contract RelayerConsumer is Ownable {
     /// @notice Fetches the latest price from the price feed after checking for deviation
     /// @dev Reverts if the relayer price is not within the deviation range of the price feed price
     function getLatestPrice() public view returns (int256) {
-        int256 price = priceFeed.latestAnswer();
-        int256 relayerPrice = relayer.latestAnswer();
+        (, int256 price, , uint256 timestamp, ) = priceFeed.latestRoundData();
+        (, int256 relayerPrice, , uint256 relayerTimestamp, ) = priceFeed.latestRoundData();
+
+        require(timestamp != 0, "No data from price feed");
+        require(relayerTimestamp != 0, "No data from relayer");
+
         uint256 calculatedDeviation = uint256((abs(relayerPrice - price) * 10000) / relayerPrice);
         require(calculatedDeviation >= deviationPercent, "Relayer/price feed deviation too large");
-        return priceFeed.latestAnswer();
+        return price;
     }
 
     /// @notice Changes price feed contract address
