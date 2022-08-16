469f09b148b3eed7fadf79ea01ec7f0237e5a7b7
mnaga <mennat.abuelnaga@gmail.com>
2022-08-16 10:35:47 +0200

    fix: getLatestPrice()

--- contracts/RelayerConsumer.sol
+++ contracts/RelayerConsumer.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RelayerConsumer is Ownable {
     CLV2V3Interface public priceFeed;
     CLV2V3Interface public relayer;
-    uint256 public deviationPercent = 200; // 2%
+    uint256 public deviationPercent = 50; // 0.5%
 
     constructor(address _priceFeed, address _relayer) {
         priceFeed = CLV2V3Interface(_priceFeed);
@@ -18,13 +18,12 @@ contract RelayerConsumer is Ownable {
     /// @dev Reverts if the relayer price is not within the deviation range of the price feed price
     function getLatestPrice() public view returns (int256) {
         (, int256 price, , uint256 timestamp, ) = priceFeed.latestRoundData();
-        (, int256 relayerPrice, , uint256 relayerTimestamp, ) = priceFeed.latestRoundData();
-
+        (, int256 relayerPrice, , uint256 relayerTimestamp, ) = relayer.latestRoundData();
         require(timestamp != 0, "No data from price feed");
         require(relayerTimestamp != 0, "No data from relayer");
 
         uint256 calculatedDeviation = uint256((abs(relayerPrice - price) * 10000) / relayerPrice);
-        require(calculatedDeviation >= deviationPercent, "Relayer/price feed deviation too large");
+        require(calculatedDeviation < deviationPercent, "Relayer/price feed deviation too large");
         return price;
     }
 
