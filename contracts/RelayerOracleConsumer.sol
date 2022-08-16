942963f3219abef72c1f82d9d4d6ba022305e861
mnaga <mennat.abuelnaga@gmail.com>
2022-08-16 16:41:22 +0200

    feat: revert if prices are outdated

--- contracts/RelayerOracleConsumer.sol
+++ contracts/RelayerOracleConsumer.sol
@@ -4,39 +4,44 @@ pragma solidity ^0.8.12;
 import "./interface/CLV2V3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RelayerConsumer is Ownable {
+contract RelayerOracleConsumer is Ownable {
     CLV2V3Interface public priceFeed;
-    CLV2V3Interface public relayer;
+    CLV2V3Interface public relayerFeed;
     uint256 public deviationPercent = 50; // 0.5%
+    uint256 public maxDelay = 15 * 60; // 15 mins
 
     constructor(address _priceFeed, address _relayer) {
         priceFeed = CLV2V3Interface(_priceFeed);
-        relayer = CLV2V3Interface(_relayer);
+        relayerFeed = CLV2V3Interface(_relayer);
     }
 
     /// @notice Fetches the latest price from the price feed after checking for deviation
-    /// @dev Reverts if the relayer price is not within the deviation range of the price feed price
+    /// @dev Reverts if prices deviated or are outdated
     function getLatestPrice() public view returns (int256) {
         (, int256 price, , uint256 timestamp, ) = priceFeed.latestRoundData();
-        (, int256 relayerPrice, , uint256 relayerTimestamp, ) = relayer.latestRoundData();
-        require(timestamp != 0, "No data from price feed");
-        require(relayerTimestamp != 0, "No data from relayer");
+        (, int256 relayerPrice, , uint256 relayerTimestamp, ) = relayerFeed.latestRoundData();
+
+        require((timestamp != 0) && (block.timestamp - timestamp < maxDelay), "No/old data from price feed");
+        require(
+            (relayerTimestamp != 0) && (block.timestamp - relayerTimestamp < maxDelay),
+            "No/old data from relayer feed"
+        );
 
         uint256 calculatedDeviation = uint256((abs(relayerPrice - price) * 10000) / relayerPrice);
         require(calculatedDeviation < deviationPercent, "Relayer/price feed deviation too large");
         return price;
     }
 
-    /// @notice Changes price feed contract address
+    /// @notice Changes pricefeed contract address
     /// @dev Only callable by the owner
     function setPriceFeed(address _priceFeed) external onlyOwner {
         priceFeed = CLV2V3Interface(_priceFeed);
     }
 
-    /// @notice Changes relayer feed contract address
+    /// @notice Changes relayerfeed contract address
     /// @dev Only callable by the owner
     function setRelayerFeed(address _relayer) external onlyOwner {
-        relayer = CLV2V3Interface(_relayer);
+        relayerFeed = CLV2V3Interface(_relayer);
     }
 
     /// @notice Sets consumer contract deviationPercent variable
