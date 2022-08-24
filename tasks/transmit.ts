fff37ae89055ddc8fe26bc779a9cc71ecb8079f6
mnaga <mennat.abuelnaga@gmail.com>
2022-08-24 17:13:24 +0200

    chore: add fetchDeviation task

--- tasks/transmit.ts
+++ tasks/transmit.ts
@@ -1,5 +1,5 @@
 import { task } from "hardhat/config";
-
+// npx hardhat transmit --network aurora-testnet --contract 0x5d5d3493172Edec1748810287258c2eD09ad74A1 --answer 1600
 task("transmit", "Submits an answer to a price feed")
   .addParam("contract", "The price feed contract to post to")
   .addParam("answer", "The answer to post")
